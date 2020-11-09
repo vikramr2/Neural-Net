cb30b679bde8437a500a29f6a5cbe2cffa324cf5
Vikram Ramavarapu <Ramaavi2019@gmail.com>
2020-11-08 22:52:22 -0600

    MNIST shell script

---
+++ download-MNIST.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Download Datasets
+curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+
+# Unzip Data
+gunzip t*-ubyte.gz
