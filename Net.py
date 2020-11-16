Backprop fixed and accuracy boosted

Net.py
@@ -37,11 +37,11 @@ class Net:
         upstreams = []
         grad = np.zeros(len(self.out))
         
-        for k in range(len(self.out)):
-            kronecker = 1 if y == k else 0
-            grad[k] = -(kronecker - self.out[y])
+        for i in range(len(grad)):
+            kronecker = 1 if y == i else 0
+            grad[i] = self.out[i] - kronecker
 
-        for n in range(len(self.layers) - 2, -1, -1):
+        for n in range(len(self.layers) - 1, -1, -1):
             if isinstance(self.layers[n], Linear):
                 streams = self.layers[n].backward(grad)
                 

Optimizers.py
@@ -1,21 +1,15 @@
 import numpy as np
 
 class SGD:
-    def __init__(self, lr, decay=0, momentum=0, friction=0.5, set_size=1):
+    def __init__(self, lr, decay=0):
         self.lr = lr
-        self.epoch = 0
+        self.iter = 0
         self.rate = decay
-        self.m = momentum
-        self.beta = friction
-        self.set = set_size
 
     def __call__(self, weights, upstream):
-        curr_lr = self.lr * np.exp(-self.rate*self.epoch)
-        
-        M = np.full(upstream.shape, self.beta * self.m)
-        M = M - curr_lr * upstream
+        curr_lr = self.lr * np.exp(-self.rate*self.iter)
 
-        return weights - self.lr * upstream
+        return weights - curr_lr * upstream
 
     def advance(self):
-        self.epoch += 1
+        self.iter += 1